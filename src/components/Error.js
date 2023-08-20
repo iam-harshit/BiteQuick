95921ff68225e8533859d4d9ce917f1a3686a4c8
iam-harshit <importantemail545@gmail.com>
2023-08-20 15:22:38 +0530

    Style error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,17 +1,24 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const Error = () => {
+  const err = useRouteError();
 
-    const err = useRouteError();
-    console.log(err);
-
-    return(
-        <div>
-            <h1>Opps!!</h1>
-            <h2>Something went wrong</h2>
-            <h3>{err.status} {err.statusText}</h3>
-        </div>
-    );
-}
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center">
+        <h1 className="font-bold text-4xl pb-3">Opps!!</h1>
+        <h3 className="text-2xl pb-7">
+          {err.status} {err.statusText}
+        </h3>
+        <Link
+          to="/"
+          className="bg-[#8DC73F] hover:bg-[#63c132] text-white rounded-md px-4 py-2 transition duration-300 ease-in-out"
+        >
+          Go Home
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Error;

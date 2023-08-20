9541d1b7c58a596196f8812e834cce1bf5bc1a1b
iam-harshit <importantemail545@gmail.com>
2023-08-20 15:23:40 +0530

    Style grocery mart component

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,9 +1,16 @@
+import grocery_mart from "../asset/grocery-mart.png";
+
 const Grocery = () => {
-    return(
-        <div>
-            <h2>This is our online grocery store.</h2>
-        </div>
-    );
-}
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center ">
+        <img src={grocery_mart} alt="" />
+        <h2 className="font-bold text-3xl pb-7">
+          Welcome to our grocery store
+        </h2>
+      </div>
+    </div>
+  );
+};
 
 export default Grocery;
